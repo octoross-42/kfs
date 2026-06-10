9bfcd096f484341ece7171ba362f5e2bb44f04a6
octoross-42 <codingoceane@gmail.com>
2026-06-10 18:16:36 +0200

    fix(kernel.ld): proper kfs-1 linker
    now 2 segments: rw and rx, and attribution of each section to one segment

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,9 +3,8 @@
 
 void	kfs1_screen(void)
 {
-	vga_goto((VGA_WIDTH / 2) - 4, VGA_HEIGHT / 2 - 2);
-	vga_write((unsigned char *)"octor-os");
-	vga_goto(0, VGA_HEIGHT - 5);
+	vga_goto((VGA_WIDTH / 2) - 1, VGA_HEIGHT / 2 - 2);
+	vga_write((unsigned char *)"42");
 }
 
 void	kernel_main(void)
@@ -13,6 +12,6 @@ void	kernel_main(void)
 	vga_init_screens();
 
 	kfs1_screen();
-	
+
 	active_keyboard();
 }
